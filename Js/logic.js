70d326a0a4a56a2982ea661592672784d8c90780
EricR19 <eric19rr@outlook.es>
2021-11-10 17:01:54 -0600

    Finish second Function UpperCase

--- Js/logic.js
+++ Js/logic.js
@@ -5,3 +5,15 @@ function isEven(num) {
     return `Es impar el numero ${num}`;
   }
 }
+console.log(isEven(8));
+
+function upperCase(str) {
+  str = str.replace(/\s/g, "");
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+      return `El texto contiene mayusculas`;
+    }
+  }
+  return `El texto no contiene mayusculas  ${"'" + str + "'"}`;
+}
+console.log(upperCase("b q b"));
