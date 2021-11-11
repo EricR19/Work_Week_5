968aa5acccb8f32fa9454757f63af9026042d0cc
Eric <eric19rr@outlook.es>
2021-11-10 23:03:47 -0600

    Finish Logic

--- Js/logic.js
+++ Js/logic.js
@@ -1,30 +1,41 @@
-function isEven(num) {
-  if (num % 2 === 0) {
-    return `Es par el numero ${num}`;
-  } else {
-    return `Es impar el numero ${num}`;
-  }
+const isEven = num => {
+    if (num % 2 === 0) {
+        return `Es par el numero ${num}`;
+    } else {
+        return `Es impar el numero ${num}`;
+    }
 }
-console.log(isEven(8));
+console.log(isEven(1));
 
 function upperCase(str) {
-  str = str.replace(/\s/g, "");
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) === str.charAt(i).toUpperCase()) {
-      return `El texto contiene mayusculas`;
+    str = str.replace(/\s/g, "");
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+            return `El texto contiene mayusculas`;
+        }
     }
-  }
-  return `El texto no contiene mayusculas  ${"'" + str + "'"}`;
+    return `El texto no contiene mayusculas  ${"'" + str + "'"}`;
 }
 console.log(upperCase("ñ"));
 
 function palindromo(text) {
-  text = text.toLowerCase();
-  let normalText = text;
-  var normalizeText = normalText
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-  console.log(normalizeText);
+    text = text.toLowerCase();
+    let arrayString = [];
+    let normalText = text;
+    let normalizeText = normalText
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
+    normalizeText = normalizeText.replace(/\s/g, "");
+    arrayString = normalizeText.split('');
+    console.log(arrayString);
+    let reverseText = arrayString.reverse();
+
+    if (normalizeText == reverseText.toString().replace(/,/g, "")) {
+        console.log('si');
+    } else {
+        console.log('No');
+    }
 }
 
-palindromo("Esto es una prueba");
+palindromo("Aman a Panamá");
