5b4a110c1230108133cf5638ea786f91bd7ae637
Eric <eric19rr@outlook.es>
2021-12-25 11:56:35 -0600

    Changes things

--- Js/logic.js
+++ Js/logic.js
@@ -1,135 +1,140 @@
-/*Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar.*/
+/*Write the code of a function that is passed an integer as a parameter and returns a text string that indicates whether the number is odd or even.*/
 const isEven = (num) => {
-  if (num % 2 === 0) {
-    document.getElementById(
-      "display-info"
-    ).innerHTML = `Es par el numero ${num}`;
-  } else {
-    document.getElementById(
-      "display-info"
-    ).innerHTML = `Es impar el numero ${num}`;
-  }
-};
-/*Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
-function upperCase(str) {
-  let normal = str;
-  let upper = str.toUpperCase().replace(/\s/g, "");
-  str = str.replace(/\s/g, "");
-  if (str == upper) {
-    document.getElementById(
-      "display-info"
-    ).innerHTML = `El texto contiene solo mayúsculas  ${"'" + normal + "'"}`;
-  } else {
-    for (let i = 0; i < str.length; i++) {
-      if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+    if (num % 2 === 0) {
         document.getElementById(
-          "display-info"
-        ).innerHTML = `El texto contiene mayúsculas y minúsculas  ${
+            "display-info"
+        ).innerHTML = `The number is even ${num}`;
+    } else {
+        document.getElementById(
+            "display-info"
+        ).innerHTML = `The number is odd ${num}`;
+    }
+};
+/*Define a function that returns information about a text string passed to it as an argument. From the string passed to it, the function determines whether that string is made up of only uppercase letters, only lowercase letters, or a mixture of both.*/
+function upperCase(str) {
+    let normal = str;
+    let upper = str.toUpperCase().replace(/\s/g, "");
+    str = str.replace(/\s/g, "");
+    if (str == upper) {
+        document.getElementById(
+            "display-info"
+        ).innerHTML = `The text contains only Uppercase  ${"'" + normal + "'"}`;
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+                document.getElementById(
+                    "display-info"
+                ).innerHTML = `The text contains Uppercase and Lowercase  ${
           "'" + normal + "'"
         }`;
-        break;
-      }
-      document.getElementById(
-        "display-info"
-      ).innerHTML = `El texto no contiene mayúsculas  ${"'" + normal + "'"}`;
+                break;
+            }
+            document.getElementById(
+                "display-info"
+            ).innerHTML = `The text contains only Lowercase  ${"'" + normal + "'"}`;
+        }
     }
-  }
 }
-/*Definir una función que determine si la cadena de texto que se le pasa como parámetro es un palíndromo, es decir, si se lee de la misma forma desde la izquierda y desde la derecha. Ejemplo:"Yo dono rosas, oro no doy"*/
+/*Define a function that determines if the text string that is passed as a parameter is a palindrome, that is, if it is read in the same way from the left and from the right.*/
 function palindromo(text) {
-  let normalText = text;
-  text = text.toLowerCase();
-  let arrayString = [];
+    let normalText = text;
+    text = text.toLowerCase();
+    let arrayString = [];
 
-  let normalizeText = text
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f-\u002C]/g, "");
+    let normalizeText = text
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f-\u002C]/g, "");
 
-  normalizeText = normalizeText.replace(/\s/g, "");
-  arrayString = normalizeText.split("");
-  let reverseText = arrayString.reverse();
+    normalizeText = normalizeText.replace(/\s/g, "");
+    arrayString = normalizeText.split("");
+    let reverseText = arrayString.reverse();
 
-  if (normalizeText == reverseText.toString().replace(/,/g, "")) {
-    document.getElementById("display-info").innerHTML = `El texto ( ${
+    if (normalizeText == reverseText.toString().replace(/,/g, "")) {
+        document.getElementById("display-info").innerHTML = `The text ( ${
       "" + normalText + ""
-    } ) es un palíndromo`;
-  } else {
-    document.getElementById("display-info").innerHTML = `El texto ( ${
+    } ) is a palindrome`;
+    } else {
+        document.getElementById("display-info").innerHTML = `The text ( ${
       "" + normalText + ""
-    } ) no es un palíndromo`;
-  }
+    } ) is not a palindrome`;
+    }
 }
 
-//#region DOM Elementos Creados
+//#region DOM Elements
 
 function createElementTittle(titulo) {
-  document.getElementById("tittle-1").innerHTML = titulo;
+    document.getElementById("tittle-1").innerHTML = titulo;
 }
+
 function createinfoLabel(info) {
-  document.getElementById("info").innerHTML = info;
+    document.getElementById("info").innerHTML = info;
 }
 
 function createButton() {
-  let take = document.getElementById("button-info");
-  const button = document.createElement("button");
-  button.innerHTML = "Obtener Resultado";
-  button.setAttribute("class", "set-button");
-  button.setAttribute("id", "set-button");
-  take.appendChild(button);
-  return button;
+    let take = document.getElementById("button-info");
+    const button = document.createElement("button");
+    button.innerHTML = "Get Result";
+    button.setAttribute("class", "set-button");
+    button.setAttribute("id", "set-button");
+    take.appendChild(button);
+    return button;
 }
+
 function createInput() {
-  let take = document.getElementById("input");
-  const input = document.createElement("input");
-  input.setAttribute("id", "data");
-  take.appendChild(input);
+    let take = document.getElementById("input");
+    const input = document.createElement("input");
+    input.setAttribute("id", "data");
+    take.appendChild(input);
 }
 
 function createIsEven() {
-  createElementTittle("Números Pares o Impares");
-  createinfoLabel("Inserte un número si deseas saber si es par o impar");
-  clear();
-  createInput();
-  let button = createButton();
-  button.onclick = function () {
-    let data = document.getElementById("data").value;
-    isEven(data);
-  };
+    createElementTittle("Even and odd numbers");
+    createinfoLabel("Insert a number to know if is even or odd");
+    clear();
+    createInput();
+    let button = createButton();
+    button.onclick = function() {
+        let data = document.getElementById("data").value;
+        isEven(data);
+    };
 }
-function upperCases() {
-  createElementTittle("Letras mayúsculas ");
-  createinfoLabel(
-    "Ingrese una cadena de texto e indicará si contiene o no mayúsculas"
-  );
 
-  clear();
-  createInput();
-  let button = createButton();
-  button.onclick = function () {
-    let data = document.getElementById("data").value;
-    upperCase(data);
-  };
+function upperCases() {
+    createElementTittle("Uppercase and Lowercase");
+    createinfoLabel(
+        "Enter a text string and it will indicate if it contains uppercase or lowercase letters or both"
+    );
+
+    clear();
+    createInput();
+    let button = createButton();
+    button.onclick = function() {
+        let data = document.getElementById("data").value;
+        upperCase(data);
+    };
 }
+
 function palindromoText() {
-  createElementTittle("Palabras o Frases Palíndromas");
-  createinfoLabel(
-    "Ingrese una cadena de texto o frase para saber si es Palíndroma"
-  );
-  clear();
-  createInput();
-  let button = createButton();
-  button.onclick = function () {
-    let data = document.getElementById("data").value;
-    palindromo(data);
-  };
+    createElementTittle("Palindromic words or phrases");
+    createinfoLabel(
+        "Enter a word or phrase to know if is Palindromic"
+    );
+    clear();
+    createInput();
+    let button = createButton();
+    button.onclick = function() {
+        let data = document.getElementById("data").value;
+        palindromo(data);
+    };
 }
+
 function clear() {
-  let btn = document.getElementById("set-button");
-  let inp = document.getElementById("data");
-  if (btn != null) {
-    inp.remove();
-    btn.remove();
-    document.getElementById("display-info").innerHTML = "";
-  }
+    let btn = document.getElementById("set-button");
+    let inp = document.getElementById("data");
+    if (btn != null) {
+        inp.remove();
+        btn.remove();
+        document.getElementById("display-info").innerHTML = "";
+    }
 }
 //#endregion
